bf811c4b27c021a65128ec481734aee4c65e199e
Robbie Ferguson <robbiejferguson92@gmail.com>
2021-11-25 15:02:23 +0000

    second commit

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,8 +1,14 @@
 from flask import render_template, request
 from app import app
-# from models.event import Event
+from models.event import Event
+from models.event_list import events
 
 
 @app.route("/")
 def index():
-    return "hello world"
+    return render_template("index.html", title = "Home", events = events)
+
+@app.route("/add_event")
+def add_event():
+    return render_template("add_event.html", title = "Add Event")
+    
---
+++ models/event_list.py
@@ -0,0 +1,9 @@
+from models.event import *
+
+event_1 = Event("25/05/2031", "CodeClan Reunion", 18, "The G28 Hall", "A reunion for the G28 class of 2021/2022")
+event_2 = Event("06/06/2032", "Annual Bakers Conference", 26, "The Green Kitched", "The world's foremost bakers get together for chat.")
+
+events = [event_1, event_2]
+
+def add_event(event):
+    events.append(event)
